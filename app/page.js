986c6b642d4244d80cbf1957bf286d42c6e3a4e1
manismk <manismkmech@gmail.com>
2022-12-26 19:33:11 +0530

    chore: intial setup and clean up

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Box display="flex" justifyContent="center" alignItems="center" p="1rem">
+        <Box>Hello world</Box>
+      </Box>
+    </>
+  );
+}
