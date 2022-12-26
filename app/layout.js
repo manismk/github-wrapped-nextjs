b9b7ab241a7b92f8d84c099bb02a816a63efb2f4
manismk <manismkmech@gmail.com>
2022-12-26 20:55:36 +0530

    chore: next js 13 changes

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,15 @@
+"use client";
+import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
+import { theme } from "../styles/theme";
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </body>
     </html>
   );
 }
---
+++ styles/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#f3f4fa",
+        color: "rgb(59, 55, 191)",
+      },
+    },
+  },
+});
