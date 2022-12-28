2ab15da6a54b768f0f9b26db3f0a7620cc654e37
manismk <manismkmech@gmail.com>
2022-12-28 18:55:08 +0530

    refactor: added empty og image

--- app/head.js
+++ app/head.js
@@ -10,6 +10,29 @@ export default function Head() {
       />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://githubwrapped.netlify.app/" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta propert="twitter:creator" content="@_manismk" />
+      <meta
+        property="twitter:url"
+        content="https://githubwrapped.netlify.app/"
+      />
+      <meta property="twitter:title" content={`Github Wrapped`} />
+      <meta
+        property="twitter:description"
+        content="GithubWrapped 2022 -  Get your Total contriburtion, Active days, longest streak, Most active day, month and more"
+      />
+      <meta
+        name="twitter:image:src"
+        content={`https://githubwrapped.netlify.app/api/og?username=`}
+      />
+      <meta
+        property="twitter:image"
+        content={`https://githubwrapped.netlify.app/api/og?username=`}
+      />
+      <meta
+        property="og:image"
+        content={`https://githubwrapped.netlify.app/api/og?username=`}
+      />
     </>
   );
 }
