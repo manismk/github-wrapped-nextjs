dbceb4cc4cd42433be02a9d6c88dba6066953e17
manismk <manismkmech@gmail.com>
2022-12-28 20:43:19 +0530

    feat: added twitter share button

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -2,7 +2,9 @@
 import {
   Box,
   Button,
+  Center,
   Flex,
+  Icon,
   IconButton,
   Link,
   Spinner,
@@ -14,6 +16,7 @@ import { Result } from "../../components/Result";
 import { HiDownload } from "react-icons/hi";
 import { useRouter } from "next/navigation";
 import { exportComponentAsPNG } from "react-component-export-image";
+import { FaTwitter } from "react-icons/fa";
 
 const User = (page) => {
   const [data, setData] = useState({});
@@ -86,6 +89,14 @@ const User = (page) => {
                 })
               }
             />
+            <Link
+              href={`https://twitter.com/intent/tweet?original_referer=https://githubwrapped.netlify.app&text=In 2022, I made over ${data?.totalCount} commits, was active ${data?.activeDaysCount}/365 days with longest streak of ${data?.longestStreak?.streak}days! Check your %23GithubWrapped %23GithubWrapped2022 👇&tw_p=tweetbutton&url=githubwrapped.netlify.app`}
+              target="_blank"
+            >
+              <Center bg="#fff" w="40px" h="40px" borderRadius="5px">
+                <Icon as={FaTwitter} />
+              </Center>
+            </Link>
             <Button
               bg="#fff"
               _hover={{ background: "#fff" }}
