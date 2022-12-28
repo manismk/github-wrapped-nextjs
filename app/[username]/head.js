ecad63aae566d95adc04834baef9bea45636c829
manismk <manismkmech@gmail.com>
2022-12-28 19:17:23 +0530

    refactor: username route og refactor

--- app/[username]/head.js
+++ app/[username]/head.js
@@ -1,13 +1,9 @@
 export default function Head({ params }) {
   return (
     <>
-      <title>{`${params.username} | Github Wrapped`}</title>
+      <title>Github Wrapped</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-      <meta
-        property="og:image"
-        content={`https://githubwrapped.netlify.app/api/og?username=${params.username}`}
-      />
       <meta
         name="description"
         content={`GithubWrapped 2022 -  Get your Total contriburtion, Active days, longest streak, Most active day, month and more`}
@@ -20,10 +16,7 @@ export default function Head({ params }) {
         property="twitter:url"
         content="https://githubwrapped.netlify.app/"
       />
-      <meta
-        property="twitter:title"
-        content={`${params.username} | Github Wrapped`}
-      />
+      <meta property="twitter:title" content="Github Wrapped" />
       <meta
         property="twitter:description"
         content="GithubWrapped 2022 -  Get your Total contriburtion, Active days, longest streak, Most active day, month and more"
@@ -34,6 +27,10 @@ export default function Head({ params }) {
       />
       <meta
         property="twitter:image"
+        content={`https://githubwrapped.netlify.app/api/og?username=${params.username}`}
+      />
+      <meta
+        property="og:image"
         content={`https://githubwrapped.netlify.app/api/og?username=${params.username}`}
       />
     </>
