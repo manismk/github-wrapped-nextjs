715e1e8596e10d2c6a85b32039786ae1439b9b2e
Manikandan S <manismkmech@gmail.com>
2024-11-03 10:55:26 +0530

    feat: handle same page result

--- app/page.js
+++ app/page.js
@@ -9,26 +9,67 @@ import {
   InputLeftElement,
   Text,
 } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 import { FaGithub } from "react-icons/fa";
+import { BottomBar, Brand, Result } from "../components";
 
 export default function Home() {
   const inputRef = useRef(null);
   const [error, setError] = useState(null);
-  const router = useRouter();
   const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
   const currentYear = process.env.NEXT_PUBLIC_CURR_YEAR || 2022;
+  const resultRef = useRef(null);
 
-  const clickHandler = () => {
-    if (inputRef?.current?.value?.trim()?.length) {
+  const clickHandler = async () => {
+    const username = inputRef?.current?.value?.trim();
+    if (username?.length) {
       setLoading(true);
-      router.push(`/${inputRef?.current?.value?.trim()}`);
+      try {
+        const res = await fetch("/api/getData", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username }),
+        });
+        if (res.status === 200) {
+          const body = await res.json();
+          setData(body);
+          setLoading(false);
+        } else {
+          const text = await res.text();
+          setError(text);
+          setLoading(false);
+        }
+      } catch (e) {
+        setError("Something went wrong!");
+        setLoading(false);
+      }
     } else {
       setError("Please enter valid github username");
       inputRef.current.value = "";
     }
   };
+
+  if (data) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" p="1rem">
+        <Box mt="3rem" textAlign="center">
+          <Box boxShadow="xl">
+            <Box p="2rem" pb="0.5rem" bg="#f3f4fa" ref={resultRef}>
+              <Result data={data} />
+              <Brand />
+            </Box>
+            <BottomBar
+              currentYear={currentYear}
+              data={data}
+              resultRef={resultRef}
+              onAnotherUserClick={() => setData(null)}
+            />
+          </Box>
+        </Box>
+      </Box>
+    );
+  }
 
   return (
     <>
